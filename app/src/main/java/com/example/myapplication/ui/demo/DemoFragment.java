37dcbb20f023c60425ba5d9dbd1adf72eaad5dee
customized811 <tom.p.hopp@gmail.com>
2021-04-15 12:53:39 +0200

    Whatsapp Video Call in DemoFragment zum Testen

--- app/src/main/java/com/example/myapplication/ui/demo/DemoFragment.java
+++ app/src/main/java/com/example/myapplication/ui/demo/DemoFragment.java
@@ -1,38 +1,81 @@
 package com.example.myapplication.ui.demo;
 
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import com.example.myapplication.R;
+import com.example.myapplication.ui.masterdata.MasterdataViewModel;
 
 public class DemoFragment extends Fragment {
 
-    private DemoViewModel mViewModel;
-
-    public static DemoFragment newInstance() {
-        return new DemoFragment();
-    }
+    private DemoViewModel demoViewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_demo, container, false);
+        demoViewModel =
+                new ViewModelProvider(this).get(DemoViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_demo, container, false);
+
+        askPermissions();
+
+        Button button = root.findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    askPermissions();
+                } else {
+                    String ID = "015206417298";
+                    if (ID != null) {
+                        videoCall(ID);
+                    } else {
+                        Toast.makeText(v.getContext(), "Number not found or can't accept video call!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        return root;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(DemoViewModel.class);
-        // TODO: Use the ViewModel
+    // Get required permissions
+    public void askPermissions() {
+        // Ask permissions
+        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 1);
     }
+
+    //Video Call Intent
+    public void videoCall(String ID) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        String data = "content://com.android.contacts/data/" + ID;
+        String type = "vnd.android.cursor.item/vnd.com.whatsapp.video.call";
+        intent.setDataAndType(Uri.parse(data), type);
+        intent.setPackage("com.whatsapp");
+        startActivity(intent);
+    }
+
 
 }
--- app/src/main/java/com/example/myapplication/ui/demo/DemoViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/demo/DemoViewModel.java
@@ -1,7 +1,18 @@
 package com.example.myapplication.ui.demo;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class DemoViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<String> mText;
+
+    public DemoViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is emergency fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
